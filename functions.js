9210d06d6bb69a353800ca7a61b5d4fdf7913434
Siyuan Li <jacklisiyuan@gmail.com>
2022-06-20 22:08:49 +1000

    read more ftn updated

--- functions.js
+++ functions.js
@@ -7,36 +7,27 @@ const kdiv = document.querySelector('#kdiv');
 const pdiv = document.querySelector('#pdiv');
 
 //function to show the long story
-function readMoreBtnK() {
+const readMoreBtnK = () => {
     /**
      * 默认按键字为'Read More'
      * 如果长故事为不显示（默认情况），则显示长故事，并且改变按键字为'Read Less'
      */
+    kdiv.style.display === 'none'
+        ? (kdiv.style.display = 'block') &&
+          (KReadMore.innerHTML = 'Read Less')
+        : //此时长故事已经显示，再次点击，则不显示，并且改变按键字为'Read More'
+          (kdiv.style.display = 'none') &&
+          (KReadMore.innerHTML = 'Read More');
+};
 
-    if (kdiv.style.display === 'none') {
-        KReadMore.innerHTML = 'Read Less';
-        kdiv.style.display = 'block';
-        /**
-         * 此时长故事已经显示，再次点击，则不显示，并且改变按键字为'Read More'
-         */
-    } else {
-        KReadMore.innerHTML = 'Read More';
-        kdiv.style.display = 'none';
-    }
-}
-
-function readMoreBtnP() {
-    if (pdiv.style.display === 'none') {
-        PReadMore.innerHTML = 'Read Less';
-        pdiv.style.display = 'block';
-        /**
-         * 此时长故事已经显示，再次点击，则不显示，并且改变按键字为显示
-         */
-    } else {
-        PReadMore.innerHTML = 'Read More';
-        pdiv.style.display = 'none';
-    }
-}
+const readMoreBtnP = () => {
+    pdiv.style.display === 'none'
+        ? (pdiv.style.display = 'block') &&
+          (PReadMore.innerHTML = 'Read Less')
+        : //此时长故事已经显示，再次点击，则不显示，并且改变按键字为'Read More'
+          (pdiv.style.display = 'none') &&
+          (PReadMore.innerHTML = 'Read More');
+};
 
 // Event listener
 KReadMore.addEventListener('click', readMoreBtnK);
